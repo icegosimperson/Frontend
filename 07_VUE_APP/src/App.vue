581ac066d4c087e307fcf9614cffda95be2951fd
Hyewon Lee <hooho931@gmail.com>
2025-03-25 16:12:01 +0900

    20250325 Vue Component

---
+++ 07_VUE_APP/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ColorEmit from './components/ColorEmit.vue';
+
+</script>
+
+<template>
+  <div :style="{ backgroundColor: color }">
+  <h1>저의 색상을 정해주세요!!</h1>
+  <ColorEmit @colorChanged="colorChangeHandler"/>
+  </div>
+  </template>
+    <script>
+    export default {
+        name: 'App',
+          data() {
+            return {
+            color: '',
+          };
+          },
+          methods:{
+              colorChangeHandler(e){
+                this.color = e.color;
+              }
+          }
+    };
+</script>
---
+++ 07_VUE_APP/src/components/PropsList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h2>{{ siteName }}</h2>
+      <hr />
+      <a :href="siteUrl">{{ siteName }}</a>
+      <hr />
+      <h2>방문 여부: {{ visited }}</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PropsList',
+    props: ['siteName', 'siteUrl', 'visited'],
+  };
+  </script>
