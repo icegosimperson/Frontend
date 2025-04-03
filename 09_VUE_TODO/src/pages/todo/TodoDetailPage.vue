bcbef64547255d5d65ef81036d4aef4a3891a03e
Hyewon Lee <hooho931@gmail.com>
2025-04-03 17:44:35 +0900

    250403 Vue-todo

---
+++ 09_VUE_TODO/src/pages/todo/TodoDetailPage.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>TODO DETAIL</h1>
+    </div>
+        <h2>id: {{todo.id}}/</h2>
+        <h2>todo: {{todo.todo}}/</h2>
+        <h2>desc: {{todo.desc}}/</h2>
+        <h2>done: {{todo.done}}/</h2>
+        <button @click="router.push({name:'todo'})">목록</button>
+        <button>수정</button>
+        <button>삭제</button>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+import { RouterLink, useRoute, useRouter } from 'vue-router';
+
+const router = useRouter();
+const curRoute = useRoute();
+const id = curRoute.params.id;
+const BASE_URL = '/api';
+
+const todo = ref({}); // 반응형 데이터***
+
+async function fetchTodo() {
+  try {
+    const todoUrl = BASE_URL + `/todos/${id}`;
+    const todoRes = await axios.get(todoUrl);
+    
+    todo.value = todoRes.data; // 받아온 배열 값을 reactive X, ref로 처리
+    console.log('TODO DETAIL 데이터 목록 :', todo.value);
+
+  } catch (e) {
+    alert('TODO DETAIL 통신 ERR 발생');
+    console.log(e);
+  }
+}
+
+fetchTodo();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 09_VUE_TODO/src/pages/todo/TodoEditPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>TODO WRITE</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
